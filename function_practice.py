082475e5ba258eeb10fac38d0d35cf66865a9a0e
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-09-06 14:19:01 +0100

    End of chapter 1 page 10

---
+++ function_practice.py
@@ -0,0 +1,14 @@
+def greet(name):
+    print(f"Hi, {name}!")
+
+def add_2_numbers(x, y):
+    return x + y
+
+def add_3_numbers(x, y, z):
+    return x + y + z
+
+def concatenate_names(name1, name2, name3):
+    print("My friends are: " + name1 + ", " + name2 + " and " + name3)
+
+def seconds_per_week(weeks):
+    return weeks * 7 * 24 * 360
